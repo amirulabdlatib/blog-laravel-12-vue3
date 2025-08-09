d099d15197d11168f006d30fa92c7026c3fd3024
Amirul <amirulal73@gmail.com>
2025-08-09 22:02:50 +0800

    Add register function

--- blog-api/app/Http/Controllers/Api/AuthController.php
+++ blog-api/app/Http/Controllers/Api/AuthController.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        return 'register';
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        $user = User::create($validated);
+        $token = $user->createToken($request->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
     }
 
     public function login(Request $request)
