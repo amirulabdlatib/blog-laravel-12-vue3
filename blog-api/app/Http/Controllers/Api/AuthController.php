d359d6e7a5125a4b04913ede1a52e2d39d1af6ec
Amirul <amirulal73@gmail.com>
2025-08-09 21:51:33 +0800

    Add register,login and logout

---
+++ blog-api/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        return 'register';
+    }
+
+    public function login(Request $request)
+    {
+        return 'login';
+    }
+
+    public function logout(Request $request)
+    {
+        return 'logout';
+    }
+}
