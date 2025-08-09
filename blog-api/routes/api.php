2d0fb282f63ac9205aa8f6bf951dcbe4a27eb61e
Amirul <amirulal73@gmail.com>
2025-08-09 22:12:57 +0800

    Add logout endpoint

--- blog-api/routes/api.php
+++ blog-api/routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::apiResource('posts',PostController::class);
-Route::post('/register',[AuthController::class,'register']);
-Route::post('/login',[AuthController::class,'login']);
-Route::post('/logout',[AuthController::class,'logout']);
+Route::apiResource('posts', PostController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
