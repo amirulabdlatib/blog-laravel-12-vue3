fc980b5f6a237fd35c3b8649cecfa5816c454bdc
Amirul <amirulal73@gmail.com>
2025-08-10 00:57:13 +0800

    Add register form and styling

--- blog-client/src/views/Auth/RegisterView.vue
+++ blog-client/src/views/Auth/RegisterView.vue
@@ -2,5 +2,21 @@
 <template>
   <main>
     <h1 class="title">Register a new account</h1>
+
+    <form class="w-1/2 mx-auto space-y-6" action="">
+      <div>
+        <input type="text" placeholder="Name" />
+      </div>
+      <div>
+        <input type="email" placeholder="Email" />
+      </div>
+      <div>
+        <input type="password" placeholder="Password" />
+      </div>
+      <div>
+        <input type="password" placeholder="Confirm Passowrd" />
+      </div>
+      <button class="primary-btn">Register</button>
+    </form>
   </main>
 </template>
