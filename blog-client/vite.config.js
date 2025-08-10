8a2f8bf59e27e77b86d96aa2b7dc55115a121177
Amirul <amirulal73@gmail.com>
2025-08-10 22:48:18 +0800

    Update vite config js to read .env value

--- blog-client/vite.config.js
+++ blog-client/vite.config.js
@@ -1,28 +1,30 @@
 import { fileURLToPath, URL } from "node:url";
-
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(() => {
+  const env = loadEnv("", process.cwd());
+
+  return {
+    plugins: [vue(), vueDevTools(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_API_URLL,
-        changeOrigin: true,
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
         },
       },
     },
-  },
+  };
 });
