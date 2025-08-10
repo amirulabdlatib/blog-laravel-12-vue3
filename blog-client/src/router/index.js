fc3b721a90cc9b385180ad0bd0293e7e0f4809fb
Amirul <amirulal73@gmail.com>
2025-08-10 23:22:41 +0800

    Add middleware to check authenticate user

--- blog-client/src/router/index.js
+++ blog-client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import RegisterView from "@/views/Auth/RegisterView.vue";
 import LoginView from "@/views/Auth/LoginView.vue";
+import { useAuthStore } from "@/stores/auth";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,13 +16,24 @@ const router = createRouter({
       path: "/register",
       name: "register",
       component: RegisterView,
+      meta: { guest: true },
     },
     {
       path: "/login",
       name: "login",
       component: LoginView,
+      meta: { guest: true },
     },
   ],
+});
+
+router.beforeEach(async (to, from) => {
+  const authstore = useAuthStore();
+  await authstore.getUser();
+
+  if (authstore.user && to.meta.guest) {
+    return { name: "home" };
+  }
 });
 
 export default router;
