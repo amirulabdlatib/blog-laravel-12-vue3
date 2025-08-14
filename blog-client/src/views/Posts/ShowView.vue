9338d5ff28255a231e3f92c5d848ffa66a92486a
amirulabdlatib <amirulal73@gmail.com>
2025-08-14 12:00:13 +0800

    Add check delete button for post owner

--- blog-client/src/views/Posts/ShowView.vue
+++ blog-client/src/views/Posts/ShowView.vue
@@ -1,13 +1,18 @@
 <script setup>
+import { useAuthStore } from "@/stores/auth";
 import { usePostsStore } from "@/stores/posts";
 import { ref, onMounted } from "vue";
 import { RouterLink, useRoute } from "vue-router";
 
 const route = useRoute();
 const { getPost, deletePost } = usePostsStore();
+const authStore = useAuthStore();
 const post = ref(null);
 
-onMounted(async () => (post.value = await getPost(route.params.id)));
+onMounted(async () => {
+    post.value = await getPost(route.params.id);
+    authStore.getUser();
+});
 </script>
 
 <template>
@@ -29,7 +34,7 @@ onMounted(async () => (post.value = await getPost(route.params.id)));
                 </p>
             </div>
 
-            <div>
+            <div v-if="authStore.user && authStore.user.id == post.user_id">
                 <form @submit.prevent="deletePost(post)">
                     <button class="text-red-500 font-bold px-2 py-1 border rounded transition-colors duration-300 ease-in-out hover:bg-red-500 hover:text-gray-200">Delete</button>
                 </form>
