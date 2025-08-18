3f707e30e32513402552b661a43abebfbc85e3df
amirulabdlatib <amirulal73@gmail.com>
2025-08-18 11:42:53 +0800

    Update navbar

--- blog-client/src/App.vue
+++ blog-client/src/App.vue
@@ -6,28 +6,32 @@ import { onMounted } from "vue";
 const authStore = useAuthStore();
 
 onMounted(() => {
-  authStore.getUser();
+    authStore.getUser();
 });
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/" class="nav-link">Home</RouterLink>
+    <header>
+        <nav>
+            <RouterLink to="/" class="nav-link">Home</RouterLink>
+            <div v-if="authStore.user" class="flex items-center space-x-3">
+                <p class="text-sm text-slate-300">Welcome {{ authStore.user.name }}</p>
+                <div v-if="authStore.user.is_admin">
+                    <RouterLink :to="{ name: 'create-post' }" class="nav-link">Users</RouterLink>
+                </div>
+                <div v-else>
+                    <RouterLink :to="{ name: 'create-post' }" class="nav-link">New Post</RouterLink>
+                </div>
+                <form @submit.prevent="authStore.logout()" class="nav-link">
+                    <button type="submit">Logout</button>
+                </form>
+            </div>
+            <div v-else>
+                <RouterLink to="/register" class="nav-link">Register</RouterLink>
+                <RouterLink to="/login" class="nav-link">Login</RouterLink>
+            </div>
+        </nav>
+    </header>
 
-      <div v-if="authStore.user" class="flex items-center space-x-3">
-        <p class="text-sm text-slate-300">Welcome back {{ authStore.user.name }}</p>
-        <RouterLink :to="{ name: 'create-post' }" class="nav-link">New Post</RouterLink>
-        <form @submit.prevent="authStore.logout()" class="nav-link">
-          <button type="submit">Logout</button>
-        </form>
-      </div>
-      <div v-else>
-        <RouterLink to="/register" class="nav-link">Register</RouterLink>
-        <RouterLink to="/login" class="nav-link">Login</RouterLink>
-      </div>
-    </nav>
-  </header>
-
-  <RouterView />
+    <RouterView />
 </template>
