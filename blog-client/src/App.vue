493649486592cf28e4bfdff4ed7f1afc5a280cc4
amirulabdlatib <amirulal73@gmail.com>
2025-08-18 11:51:22 +0800

    Refactor navbar into navbar component

--- blog-client/src/App.vue
+++ blog-client/src/App.vue
@@ -2,6 +2,7 @@
 import { RouterLink, RouterView } from "vue-router";
 import { useAuthStore } from "./stores/auth";
 import { onMounted } from "vue";
+import Navbar from "./components/Navbar.vue";
 
 const authStore = useAuthStore();
 
@@ -12,25 +13,7 @@ onMounted(() => {
 
 <template>
     <header>
-        <nav>
-            <RouterLink to="/" class="nav-link">Home</RouterLink>
-            <div v-if="authStore.user" class="flex items-center space-x-3">
-                <p class="text-sm text-slate-300">Welcome {{ authStore.user.name }}</p>
-                <div v-if="authStore.user.is_admin">
-                    <RouterLink :to="{ name: 'create-post' }" class="nav-link">Users</RouterLink>
-                </div>
-                <div v-else>
-                    <RouterLink :to="{ name: 'create-post' }" class="nav-link">New Post</RouterLink>
-                </div>
-                <form @submit.prevent="authStore.logout()" class="nav-link">
-                    <button type="submit">Logout</button>
-                </form>
-            </div>
-            <div v-else>
-                <RouterLink to="/register" class="nav-link">Register</RouterLink>
-                <RouterLink to="/login" class="nav-link">Login</RouterLink>
-            </div>
-        </nav>
+        <Navbar :authStore="authStore" />
     </header>
 
     <RouterView />
---
+++ blog-client/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+defineProps({
+    authStore: Object,
+});
+</script>
+
+<template>
+    <nav>
+        <RouterLink to="/" class="nav-link">Home</RouterLink>
+        <div v-if="authStore.user" class="flex items-center space-x-3">
+            <p class="text-sm text-slate-300">Welcome {{ authStore.user.name }}</p>
+            <div v-if="authStore.user.is_admin">
+                <RouterLink :to="{ name: 'create-post' }" class="nav-link">Users</RouterLink>
+            </div>
+            <div v-else>
+                <RouterLink :to="{ name: 'create-post' }" class="nav-link">New Post</RouterLink>
+            </div>
+            <form @submit.prevent="authStore.logout()" class="nav-link">
+                <button type="submit">Logout</button>
+            </form>
+        </div>
+        <div v-else>
+            <RouterLink to="/register" class="nav-link">Register</RouterLink>
+            <RouterLink to="/login" class="nav-link">Login</RouterLink>
+        </div>
+    </nav>
+</template>
