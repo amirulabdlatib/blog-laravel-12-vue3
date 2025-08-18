da02fa87f80c9f8732a33b0a770655fa78212040
amirulabdlatib <amirulal73@gmail.com>
2025-08-18 12:08:02 +0800

    Update import alias for user list page

--- blog-client/src/router/index.js
+++ blog-client/src/router/index.js
@@ -6,7 +6,7 @@ import { useAuthStore } from "@/stores/auth";
 import CreateView from "@/views/Posts/CreateView.vue";
 import ShowView from "@/views/Posts/ShowView.vue";
 import EditView from "@/views/Posts/EditView.vue";
-import IndexView from "@/views/Users/indexView.vue";
+import UsersList from "@/views/Users/indexView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -48,7 +48,7 @@ const router = createRouter({
         {
             path: "/admin/users",
             name: "users-list",
-            component: IndexView,
+            component: UsersList,
         },
     ],
 });
